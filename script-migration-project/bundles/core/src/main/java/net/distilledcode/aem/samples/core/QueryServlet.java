a060564d863e32bc9eeda4bc40b88d5fa15a6432
Soufiane El-abbadi <elabbadisoufiane@gmail.com>
2018-04-24 17:45:51 +0100

    Multifield code added and code clean

--- script-migration-project/bundles/core/src/main/java/net/distilledcode/aem/samples/core/QueryServlet.java
+++ script-migration-project/bundles/core/src/main/java/net/distilledcode/aem/samples/core/QueryServlet.java
@@ -1,24 +1,22 @@
 package net.distilledcode.aem.samples.core;
 
-import java.awt.List;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
-import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-import javax.jcr.query.Query;
-import javax.jcr.query.QueryManager;
-import javax.jcr.query.QueryResult;
-import javax.servlet.ServletException;
+
 
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+import QueryPackage.*;
+
 
 @SlingServlet(	paths = "/bin/queryservlet",
 				extensions = "html",
@@ -27,104 +25,52 @@ import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 public class QueryServlet extends SlingSafeMethodsServlet{
 
 	private static final long serialVersionUID = 1L;
-	java.util.List<Node> nodesList = new ArrayList();
-	
-	
+	List<Node> nodesList = null;
+
 	
 	@Override
-	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException{
 		ResourceResolver resourceResolver = request.getResourceResolver();
 		Session session = resourceResolver.adaptTo(Session.class);
-
+		nodesList = new ArrayList();
+		PrintWriter out = response.getWriter();
+		
+		// Query parameters
 	   	String searchPath = request.getParameter("searchPath");
-	   	String querytype = request.getParameter("querytype");
+	   	String queryType = request.getParameter("querytype");
 	   	String query = request.getParameter("query");
 	   	String operation = request.getParameter("operation");
-	  	String propertyName = request.getParameter("property-name");
-	   	String propertyType = request.getParameter("property-type");
-	   	String propertyValue = request.getParameter("property-value");
-		String removePropertyName = request.getParameter("remove-property-name");
+	   	// Retreiving data from multifield
+		String[] names = request.getParameterValues("property-name");
+		String[] types = request.getParameterValues("property-type");
+		String[] values = request.getParameterValues("property-value");
+		String[] Remvalues = request.getParameterValues("remove-property-name");
+		
+		QueryInterface queryInter = null;
+		QueryLangage queryLangage = new QueryLangage();
 	   	try {
-			nodesList = executeQuery(searchPath, querytype, query, request.getResourceResolver());
-			PrintWriter outprint = response.getWriter();
-			outprint.write("This is our counter's value ");
-			outprint.write("</br>");
-			outprint.write("This is our counter's value ");
+			
+	   		if(query.equals(null) && searchPath.equals(null)) {
+	   			query = "select * from [nt:unstructured]";
+	   			searchPath = "/content/we-retail/es/es";
+	   		}
+	   		
+		    queryInter = queryLangage.getLangageClass(queryType);
+		    nodesList = queryInter.executeQuery(searchPath, queryType, query, resourceResolver);
+			
 		   	if(operation.equals("add-modify"))
-		   		addModifyNodesProperty(nodesList, propertyName, propertyType, propertyValue, resourceResolver,outprint);
-		   	else if(operation.equals("remove")){
-		        removeNodesProperty(nodesList, removePropertyName, resourceResolver,outprint);
-		   	}
+		   		for(int i=0;i<names.length;i++)
+		   			queryInter.addModifyProperty(nodesList, names[i], types[i], values[i], resourceResolver);
+		   	else if(operation.equals("remove"))
+		   		for(int i=0;i<Remvalues.length;i++)
+		   			queryInter.removeProperty(nodesList, Remvalues[i], resourceResolver);
+		   	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	   	
 	}	
 	   	
 
-	public java.util.List<Node> executeQuery(String searchpath, String querytype, String thequery, ResourceResolver resourceResolver) throws RepositoryException{
-		   java.util.List<Node> nodeList = new ArrayList();
-		   Query q= null;
-		   Session session = resourceResolver.adaptTo(Session.class);
-		   QueryManager queryManager = session.getWorkspace().getQueryManager();
-		   
-		   if(querytype.equals("XPATH")){
-			   thequery = ("/jcr:root").concat(searchpath).concat("/").concat(thequery);
-			   q = queryManager.createQuery(thequery,Query.XPATH);
-		   }else if(querytype.equals("SQL")){
-			   if(thequery.contains("where")){
-				   thequery = thequery.concat("/").concat(" and isdescendantnode('"+searchpath+"')");
-				}else {
-					thequery = thequery.concat(" where isdescendantnode('"+searchpath+"')");
-				}
-			   q = queryManager.createQuery(thequery,Query.SQL);
-		   }else if(querytype.equals("JCR_SQL2")){
-			   if(thequery.contains("where")){
-				   thequery = thequery.concat("/").concat(" and isdescendantnode('"+searchpath+"')");
-				}else {
-					thequery = thequery.concat(" where isdescendantnode('"+searchpath+"')");
-				}
-			   q = queryManager.createQuery(thequery,Query.JCR_SQL2);
-		   }
-
-		   QueryResult result = q.execute();
-		   NodeIterator nodeIter = result.getNodes(); 
-
-		   while ( nodeIter.hasNext() ) {
-			    Node node = nodeIter.nextNode();   
-				nodeList.add(node);
-		   }      
-
-		    return nodeList;
-	   	}
-
-
-	   	public void addModifyNodesProperty(java.util.List<Node> nodList,String removePropertyName, String propertyType, String propertyValue, ResourceResolver resourceResolver, PrintWriter outprint) throws Exception{
-	   		Session session = resourceResolver.adaptTo(Session.class);
-	   		for(Node node:nodList){
-		   		Node nodeTemp = session.getNode(node.getPath());
-		   		nodeTemp.setProperty(removePropertyName, propertyValue, Integer.parseInt(propertyType));	
-		   		//nodeTemp.save();
-		   		session.save();
-	   			/* Resource resource = resourceResolver.getResource("/content/we-retail/es/es/jcr:content");
-	   			ModifiableValueMap mvm = resource.adaptTo(ModifiableValueMap.class);
-	   			mvm.put(propertyName,propertyValue);
-	   			resource.getResourceResolver().commit(); */
-			   	}
-	   		//outprint.write("This is node list size +++ "+nodList.size());
-	   		//outprint.write("This is jcr:title property value +++ "+resource.getValueMap().get("jcr:title"));
-
-	   }
-	   	   
-		public void removeNodesProperty(java.util.List<Node> nodList,String propertyName, ResourceResolver resourceResolver, PrintWriter outprint) throws Exception{
-			Session session = resourceResolver.adaptTo(Session.class);
-			for(Node node:nodList){
-			   		//outprint.write(node.getPath());
-					Node nodeTemp = session.getNode(node.getPath());
-			   		nodeTemp.getProperty(propertyName).remove();
-			   		session.save();
-			   	}
-	 
-	   }
-		
 	
 }
